1f74a42356e23bd035265b499b60bcbe2e7a273e
chadwick <cekeller6121@yahoo.com>
2017-06-07 10:01:03 -0400

    add files

---
+++ FizzBuzz.js
@@ -0,0 +1,15 @@
+
+
+// I got this one! 
+
+
+function sum(num) {
+  for (i = 0; i <= num; i++)
+   if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
