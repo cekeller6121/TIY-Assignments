0911403f5aeffabc7e9a8719e4c0b2f23a3ba210
chadwick <cekeller6121@yahoo.com>
2017-06-07 10:10:38 -0400

    added 3s and 5s, updated fizzbuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,15 +1,16 @@
 
 
-// I got this one!  
+// I got this one!
 
 
 function sum(num) {
-  for (i = 0; i <= num; i++)
+  for (i = 0; i <= num; i++) {
    if (i % 3 === 0) {
     console.log("Fizz");
   } else if (i % 5 === 0) {
     console.log("Buzz");
   } else {
     console.log(i);
+    }
   }
 }
--- Kata 3s and 5s.js
+++ Kata 3s and 5s.js
@@ -1,1 +1,9 @@
-
+function sum(num) {
+  let sumArray = [0];
+  for (let i = 0; i < num; i++) {
+    if(i % 3 === 0 || i % 5 === 0) {
+      sumArray.push(i);
+    }
+  }
+  return sumArray.reduce((a, b) => a + b, 0);
+}
